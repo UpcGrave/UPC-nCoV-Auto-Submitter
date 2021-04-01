dcd9e2fc555de442059eb941a082c3c2cc0a310f
thinson <thinson2017@outlook.com>
2021-04-01 09:43:11 +0800

    add serverchan

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ yagmail
 1. 安装依赖库
 2. 修改`info.ini`为自己的信息，`[Information]`中修改自己的账号，密码，邮件(可选，打卡完成邮件提醒),支持多账号打卡，填写时`,`隔开，`[SMTP]`处修改为自己的SMTP信息
 3. 执行` python main.py `即可完成自动填报
+4. 微信公众号推送需要[配置serverchan](https://sct.ftqq.com/)。修改程序中`SERVERCHAN_SECRETKEY`即可
 
 ## 服务器定时任务
 参考crontab 定时任务的配置
@@ -24,7 +25,9 @@ yagmail
 ## TODO
 - [x] 基本功能
 - [x] 邮件提醒
-- [ ] vx公众号提醒
+- [x] vx公众号提醒
+- [ ] github action直接运行
 
 ## History
 - 2020/12/8 基本功能和邮件功能实现
+- 2021/03/25 微信公众号推送，修复了多账号可能存在的一些bug
--- auto_covid19/main.py
+++ auto_covid19/main.py
@@ -1,7 +1,17 @@
 # coding=utf-8
 import requests, time, json, configparser, random, os, yagmail
+from utils.serverchan_push import push_to_wechat
 
-# 模拟包头
+
+
+## serverchan
+SERVERCHAN_SECRETKEY = "replace to your key"
+## [SMTP]
+SMTP_user = "replace to your info"
+SMTP_password = "replace to your info"
+SMTP_host = "replace to your info"
+
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
@@ -11,11 +21,12 @@ headers = {
 def get_time():
     return time.strftime('%Y%m%d', time.localtime(time.time()))
 
-# 帐号密码信息读取
+
+# 帐号密码信息读取,密码采用加密方式传输
 def getUserInfo():
     try:
         config = configparser.ConfigParser()
-        config.read('./info.ini')
+        config.read('/home/wukang/auto_covid19/info.ini')
         usernames = config['Information']['username'].split(',')
         passwords = config['Information']['password'].split(',')
         emails = config['Information']['email'].split(',')
@@ -27,23 +38,7 @@ def getUserInfo():
         print('get userdata failed\n %s' % e)
         return None
 
-# 帐号密码信息读取
-def getSMTPInfo():
-    try:
-        config = configparser.ConfigParser()
-        config.read('./info.ini')
-        user = config['SMTP']['user']
-        password = config['SMTP']['password']
-        host = config['SMTP']['host']
-        
-        print('get SMTP info success')
-        return user, password, host
 
-    except Exception as e:
-        print('get userdata failed\n %s' % e)
-        return None
-
-# 登录并获取old_info
 def login(userdata):
     login_url = "https://app.upc.edu.cn/uc/wap/login/check"
     session = requests.session()
@@ -63,17 +58,13 @@ def save_info(session, info):
     return save_response.text
 
 def send_email(status, info, email_address):
-    try:
-        user, password, host = getSMTPInfo()
-        yagmail_server = yagmail.SMTP(user=user, password=password, host=host)
-        email_name = [email_address]
-        email_title = [status]
-        email_content = [info]
-        yagmail_server.send(to=email_name, subject=email_title, contents=email_content)
-        yagmail_server.close()
-        print("email finished")
-    except Exception as e:
-        print(e)
+    yagmail_server = yagmail.SMTP(user=SMTP_user, password=SMTP_password, host=SMTP_host)
+    email_name = [email_address]
+    email_title = [status]
+    email_content = [info]
+    yagmail_server.send(to=email_name, subject=email_title, contents=email_content)
+    yagmail_server.close()
+    print("email finished")
 
 
 def process(userdata, email_address):
@@ -94,26 +85,53 @@ def process(userdata, email_address):
     print(save_res)
     session.close()
 
-    if email_address:
-        # 结束后发送邮件
-        if json.loads(save_res)['m'] == '操作成功':
-            send_email("[success] covid-19 submission",str(json.loads(save_res)) + '\n' + str(old_info), email_address)
-        else :
-            send_email("[failed] covid-19 submission", str(json.loads(save_res)) + '\n' + str(old_info), email_address)
+    # 结束后发送邮件
+    if json.loads(save_res)['m'] == '操作成功':
+        is_fail = 0 
+        #send_email("[success] covid-19 submission",str(json.loads(save_res)) + '\n' + str(old_info), email_address)
+        pass
+    else :
+        is_fail = 1
+        send_email("[failed] covid-19 submission", str(json.loads(save_res)) + '\n' + str(old_info), email_address)
+        pass
+    return save_res, is_fail
 
 if __name__ == "__main__":
     # 获取用户信息
     userinfos = getUserInfo()
     print(userinfos)
-
+    #userinfos = [userinfos[1]]
+    res = []
+    fail_cnt = 0
     for user in userinfos:
         print(user)
-        user_data = {
-            'username': user[0],
-            'password': user[1]
-        }
-        print(user_data)
-        process(user_data, user[2])
+        try:
+            user_data = {
+                'username': user[0],
+                'password': user[1]
+            }
+            print(user_data)
+            info, is_fail = process(user_data, user[2])
+            res.append({
+                'username' : user[0],
+                'info' : info
+            })
+            fail_cnt = fail_cnt + is_fail
+        except Exception as e:
+            print(e)
+            fail_cnt = fail_cnt + 1
+            res.append({
+                'username' : user[0],
+                'info' : e
+            })
+    # process(userdata, email_address)
+    if fail_cnt == 0:
+        res_text = "成功！！！疫情防控每日签到"
+    else:
+        res_text = "失败！！！疫情防控每日签到"
+    push_to_wechat(text = res_text,
+                    desp = str(res),
+                    secretKey = SERVERCHAN_SECRETKEY)
 
 
 
